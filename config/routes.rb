a171f94773057f95f4a167fa28029c0282193e3f
Alysson A. Costa <alysson.avila.costa@gmail.com>
2021-05-29 10:47:35 -0400

    Organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,26 @@
 Rails.application.routes.draw do
   root to: "backup#index"
-  get 'recover', to: "recover#index"
-  get 'recover/new/:name', to: "recover#new"
-  post 'recover/create'
+
+  # Volume / backup routes
   get 'volume', to: "backup#index"
-  delete 'volume/:id', to: "backup#delete"
+  
   get 'backup/new/:name', to: "backup#new"
   post 'backup/create'
-
-
-  get '/recover/download/:file', to: 'recover#download', as: 'download'
-  get '/recover/delete/:file', to: 'recover#delete'
+  
+  delete 'volume/:id', to: "backup#delete"
+  
+  # Recovery routes
+  get 'recover', to: "recover#index"
+  
+  get 'recover/new/:name', to: "recover#new"
+  post 'recover/create'
+  
   get '/recover/upload', to: 'recover#upload'
   post '/recover/upload', to: 'recover#save'
+  
+  get '/recover/download/:file', to: 'recover#download', as: 'download'
+  
+  delete '/recover/delete/:file', to: 'recover#delete'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
